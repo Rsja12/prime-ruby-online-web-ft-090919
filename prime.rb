2533d5f9b76c3ef71d8cd7beeabd62da874ca4c7
Alfonso Socarras <asocarras10@gmail.com>
2019-09-13 23:19:45 +0000

    Automatically backed up by Learn

---
+++ prime.rb
@@ -0,0 +1,15 @@
+def prime?(num)
+  
+if num <= 0 || num == 1
+  false
+else
+  collection = (2..num-1).to_a
+  collection.none? do |numero|
+    if num % numero == 0
+      true
+    else
+      false
+    end
+  end
+end
+end
